5d157657f85e71a40cbf041526cc3ffc8ecec1f1
Xavier Guertin <xavierguertin@gmail.com>
2022-11-06 17:29:30 -0500

    S341-19 - User Portal

--- userPortal.php
+++ userPortal.php
@@ -1,29 +1,179 @@
-    <?php include('./assets/header.php'); 
+<?php include('./assets/header.php'); 
 
-        $name = $_SESSION['userName'];
-        $role = $_SESSION['role'];
-        if(isset($_SESSION['userId']))
-        {
-            if (!($role == "User" or $role == "Supervisor"))
-            {
-                header("Location: ./index.php");
-                alert("You do not have permission to go on this page.");
-                exit();
-            }
-        }
-        else
+    if(isset($_SESSION['userId']))
+    {
+        if (!($role == "User" or $role == "Supervisor"))
         {
             header("Location: ./index.php");
             alert("You do not have permission to go on this page.");
             exit();
         }
-    ?>
+    }
+    else
+    {
+        header("Location: ./index.php");
+        alert("You do not have permission to go on this page.");
+        exit();
+    }
 
-    <div class="">
-        <a style="color:white;">User's Portal</a>
-    </div>
+    require_once './assets/php/dbh.php';
+    $name = $_SESSION['userName'];
+    $role = $_SESSION['role'];
+    $group = $_SESSION['groups'];
 
+    $sqlPending= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'pending'";
+    $pendingResults = $conn->query($sqlPending);
 
+    $sqlApproval= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'approval'";
+    $approvalResults = $conn->query($sqlApproval);
+
+    $sqlCompleted= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'completed'";
+    $completedResults = $conn->query($sqlCompleted);
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet">
+    
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="css/style2.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+</head>
+
+<body>
+        <!-- Jobs Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s" style="color:white;">Requests Sent</h1>
+                <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
+                    <ul class="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 ms-0 pb-3 active" data-bs-toggle="pill" href="#tab-1">
+                                <h6 class="mt-n1 mb-0" style="color:white;">Waiting</h6>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 pb-3" data-bs-toggle="pill" href="#tab-2">
+                                <h6 class="mt-n1 mb-0" style="color:white;">Approval Required</h6>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 me-0 pb-3" data-bs-toggle="pill" href="#tab-3">
+                                <h6 class="mt-n1 mb-0" style="color:white;">Completed</h6>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="tab-1" class="tab-pane fade show p-0 active">
+                            <?php
+                            if (mysqli_num_rows($pendingResults) == 0) {
+                            ?>
+                                <a style="color:white"> NOTHING TO SHOW </a>
+                            <?php
+                            }
+                            else {
+                                while($row = mysqli_fetch_assoc($pendingResults)){
+                            ?>
+                                    <div class="job-item p-4 mb-4">
+                                        <div class="row g-4">
+                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:15px;">
+                                                <div class="text-start ps-4">
+                                                    <h5 class="mb-3"><?php echo $row["title"];?></h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-35px;">
+                                                <a class="btn btn-primary" href="">Details</a>
+                                                <small class="text-truncate" style="margin-top:-30px;margin-right:75%;"><i class="far fa-calendar-alt text-primary me-2"></i>Date Created: <?php echo $row["created_at"];?></small>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                        <div id="tab-2" class="tab-pane fade show p-0">
+                            <?php
+                            if (mysqli_num_rows($approvalResults) == 0) {
+                            ?>
+                                <a style="color:white"> NOTHING TO SHOW </a>
+                            <?php
+                            }
+                            else {
+                                while($row = mysqli_fetch_assoc($approvalResults)){
+                            ?>
+                                    <div class="job-item p-4 mb-4">
+                                        <div class="row g-4">
+                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:15px;">
+                                                <div class="text-start ps-4">
+                                                    <h5 class="mb-3"><?php echo $row["title"];?></h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-35px;">
+                                                <a class="btn btn-primary" href="">Details</a>
+                                                <small class="text-truncate" style="margin-top:-30px;margin-right:75%;"><i class="far fa-calendar-alt text-primary me-2"></i>Date Created: <?php echo $row["created_at"];?></small>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                        <div id="tab-3" class="tab-pane fade show p-0">
+                            <?php
+                            if (mysqli_num_rows($completedResults) == 0) {
+                            ?>
+                                <a style="color:white"> NOTHING TO SHOW </a>
+                            <?php
+                            }
+                            else {
+                                while($row = mysqli_fetch_assoc($completedResults)){
+                            ?>
+                                    <div class="job-item p-4 mb-4">
+                                        <div class="row g-4">
+                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:15px;">
+                                                <div class="text-start ps-4">
+                                                    <h5 class="mb-3"><?php echo $row["title"];?></h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-35px;">
+                                                <a class="btn btn-primary" href="">Details</a>
+                                                <small class="text-truncate" style="margin-top:-30px;margin-right:75%;"><i class="far fa-calendar-alt text-primary me-2"></i>Date Created: <?php echo $row["created_at"];?></small>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                }
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Jobs End -->
     </div>
 
 
@@ -31,5 +181,9 @@
 <div id="footer" style="display:none;"></div>
 <!--End Of Footer-->
 </body>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 
 </html>
