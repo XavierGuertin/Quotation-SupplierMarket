011f7d060579b162d366c5294f269e1b8efe1e63
Xavier Guertin <xavierguertin@gmail.com>
2022-11-07 16:09:13 -0500

    S341-21 - Send request form to DB

--- userRequestForm.php
+++ userRequestForm.php
@@ -15,15 +15,25 @@
         alert("You do not have permission to go on this page.");
         exit();
     }
-
-	if(isset($_POST['add'])){
-		
-	}
 ?>
 		<div id="formDiv">
-			<form class = "form-center" action="" method="post"> 
+			<form class = "form-center" action="./assets/php/createRequestDB.php" method="post"> 
 				<h1> New Quotation Request </h1>
 				</br>
+				
+				<?php
+                if(isset($_GET['error']) && $_GET['error'] == 'errorSending') 
+                {
+                    echo '<div id="infoAdmin" class="alert alert-danger alert-dismissible fade show" role="alert" style="margin-left:375px;width:750px;">
+                        <strong>Error! </strong>There was an error creating the request in the Database. Try again.</div>';
+                }
+                if(isset($_GET['creation']) && $_GET['creation'] == 'success')
+                {
+                    echo '<div id="infoAdmin" class="alert alert-success alert-dismissible fade show" role="alert" style="margin-left:375px;width:750px;">
+                        <strong>Success! </strong>The request has been submitted succesfully<br>
+                        <a href="./userPortal.php"><u>Click here</u><a> to go to the portal page</div>';
+                }
+                ?>
 
 				<a for="subjectTitle"> Subject </a>
 				</br>
@@ -34,7 +44,7 @@
 				</br>
 				<textarea class="inputRequest" id="descriptionBox" name="descriptionBox" rows="10" cols="75" placeholder="Description of request"></textarea>
 				</br></br>
-				<input onclick="" type ="submit" name="add" value="Submit" class="submit-button"></input>
+				<input onclick="emptyForm();" type ="submit" name="add" value="Submit" class="submit-button"></input>
 			</form>	
 		</div>
 </div>
