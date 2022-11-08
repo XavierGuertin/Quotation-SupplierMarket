867476053dd533375216f97dc01ee4242b8672bb
Xavier Guertin <xavierguertin@gmail.com>
2022-11-07 21:05:05 -0500

    Create Supplier's Portal and Connect it to DB

--- userPortal.php
+++ userPortal.php
@@ -1,16 +1,12 @@
 <?php include('./assets/header.php'); 
 
-    if(isset($_SESSION['userId']))
-    {
-        if (!($role == "User" or $role == "Supervisor"))
-        {
+    if(isset($_SESSION['userId'])){
+        if (!($role == "User" or $role == "Supervisor")) {
             header("Location: ./index.php");
             alert("You do not have permission to go on this page.");
             exit();
         }
-    }
-    else
-    {
+    } else {
         header("Location: ./index.php");
         alert("You do not have permission to go on this page.");
         exit();
@@ -24,18 +20,18 @@
     $sqlPending= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'pending'";
     $pendingResults = $conn->query($sqlPending);
 
-    $sqlApproval= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'approval'";
-    $approvalResults = $conn->query($sqlApproval);
+    $sqlQuoted= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and (status = 'quoted' or status = 'approval')";
+    $quotedResults = $conn->query($sqlQuoted);
 
     $sqlCompleted= "SELECT * FROM heroku_8714cfa5818f328.requests WHERE groupAssigned = '$group' and status = 'completed'";
     $completedResults = $conn->query($sqlCompleted);
 ?>
 
 <body>
-        <!-- Jobs Start -->
+
         <div class="container-xxl py-5">
             <div class="container">
-                <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s" style="color:white;">Requests Sent</h1>
+                <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s" style="color:white;">User's Portal<br>Requests Sent</h1>
                 <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
                     <ul class="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
                         <li class="nav-item">
@@ -45,7 +41,7 @@
                         </li>
                         <li class="nav-item">
                             <a class="d-flex align-items-center text-start mx-3 pb-3" data-bs-toggle="pill" href="#tab-2">
-                                <h6 class="mt-n1 mb-0" style="color:white;">Approval Required</h6>
+                                <h6 class="mt-n1 mb-0" style="color:white;">Quote Received</h6>
                             </a>
                         </li>
                         <li class="nav-item">
@@ -85,24 +81,24 @@
                         </div>
                         <div id="tab-2" class="tab-pane fade show p-0">
                             <?php
-                            if (mysqli_num_rows($approvalResults) == 0) {
+                            if (mysqli_num_rows($quotedResults) == 0) {
                             ?>
                                 <a style="color:white"> NOTHING TO SHOW </a>
                             <?php
                             }
                             else {
-                                while($row = mysqli_fetch_assoc($approvalResults)){
+                                while($row = mysqli_fetch_assoc($quotedResults)){
                             ?>
                                     <div class="reqList p-4 mb-4">
                                         <div class="row g-4">
-                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:15px;">
-                                                <div class="text-start ps-4">
+                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:-55px;">
+                                                <div class="text-start ps-4" style="margin-top:45px;">
                                                     <h5 class="mb-3"><?php echo $row["subject"];?></h5>
                                                 </div>
                                             </div>
-                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-35px;">
+                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-37px;">
                                                 <a class="btn btn-primary" href="">Details</a>
-                                                <small class="text-truncate" style="margin-top:-30px;margin-right:35%;"><i class="far fa-calendar-alt text-primary me-2"></i>Date Created: <?php echo $row["created_at"];?></small>
+                                                <small class="text-truncate" style="margin-top:-30px;margin-right:35%;"><i class="far fa-calendar-alt text-primary me-2"></i>   Date Created: <?php echo $row["created_at"];?></small>
                                             </div>
                                         </div>
                                     </div>
@@ -123,14 +119,14 @@
                             ?>
                                     <div class="reqList p-4 mb-4">
                                         <div class="row g-4">
-                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:15px;">
-                                                <div class="text-start ps-4">
+                                            <div class="col-sm-12 col-md-8 d-flex align-items-center" style="margin-top:-55px;">
+                                                <div class="text-start ps-4" style="margin-top:45px;">
                                                     <h5 class="mb-3"><?php echo $row["subject"];?></h5>
                                                 </div>
                                             </div>
-                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-35px;">
+                                            <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center" style="margin-top:-37px;">
                                                 <a class="btn btn-primary" href="">Details</a>
-                                                <small class="text-truncate" style="margin-top:-30px;margin-right:35%;"><i class="far fa-calendar-alt text-primary me-2"></i>Date Created: <?php echo $row["created_at"];?></small>
+                                                <small class="text-truncate" style="margin-top:-30px;margin-right:35%;"><i class="far fa-calendar-alt text-primary me-2"></i>   Date Created: <?php echo $row["created_at"];?></small>
                                             </div>
                                         </div>
                                     </div>
